28805c024dc3bf71d3d8d76b230aec69b7514366
Nsw <aduc77716@gmail.com>
2024-10-14 10:01:11 +0700

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -79,25 +79,25 @@ public class BossBuffPlugin : TerrariaPlugin
     {
         var defaultConfig = new List<BossConfig>
         {
-            new BossConfig { id = NPCID.KingSlime, name = "King Slime", hpMultiplier = 1.5f, damageMultiplier = 1.2f },
-            new BossConfig { id = NPCID.EyeofCthulhu, name = "Eye of Cthulhu", hpMultiplier = 2.0f, damageMultiplier = 1.5f },
-            new BossConfig { id = NPCID.EaterofWorldsHead, name = "Eater of Worlds", hpMultiplier = 1.8f, damageMultiplier = 1.3f },
-            new BossConfig { id = NPCID.BrainofCthulhu, name = "Brain of Cthulhu", hpMultiplier = 1.7f, damageMultiplier = 1.4f },
-            new BossConfig { id = NPCID.QueenBee, name = "Queen Bee", hpMultiplier = 2.0f, damageMultiplier = 1.6f },
-            new BossConfig { id = NPCID.SkeletronHead, name = "Skeletron", hpMultiplier = 2.5f, damageMultiplier = 2.0f },
-            new BossConfig { id = NPCID.WallofFlesh, name = "Wall of Flesh", hpMultiplier = 3.0f, damageMultiplier = 2.5f },
-            new BossConfig { id = NPCID.Retinazer, name = "The Twins (Retinazer)", hpMultiplier = 2.2f, damageMultiplier = 1.8f },
-            new BossConfig { id = NPCID.Spazmatism, name = "The Twins (Spazmatism)", hpMultiplier = 2.2f, damageMultiplier = 1.8f },
-            new BossConfig { id = NPCID.TheDestroyer, name = "The Destroyer", hpMultiplier = 3.0f, damageMultiplier = 2.0f },
-            new BossConfig { id = NPCID.SkeletronPrime, name = "Skeletron Prime", hpMultiplier = 3.5f, damageMultiplier = 2.5f },
-            new BossConfig { id = NPCID.Plantera, name = "Plantera", hpMultiplier = 3.5f, damageMultiplier = 2.7f },
-            new BossConfig { id = NPCID.Golem, name = "Golem", hpMultiplier = 3.0f, damageMultiplier = 2.2f },
-            new BossConfig { id = NPCID.DukeFishron, name = "Duke Fishron", hpMultiplier = 4.0f, damageMultiplier = 3.0f },
-            new BossConfig { id = NPCID.CultistBoss, name = "Lunatic Cultist", hpMultiplier = 4.5f, damageMultiplier = 3.5f },
-            new BossConfig { id = NPCID.MoonLordCore, name = "Moon Lord", hpMultiplier = 5.0f, damageMultiplier = 4.0f },
-            new BossConfig { id = NPCID.QueenSlimeBoss, name = "Queen Slime", hpMultiplier = 3.0f, damageMultiplier = 2.5f },
-            new BossConfig { id = NPCID.HallowBoss, name = "Empress of Light", hpMultiplier = 4.0f, damageMultiplier = 3.5f },
-            new BossConfig { id = NPCID.Deerclops, name = "Deerclops", hpMultiplier = 2.5f, damageMultiplier = 2.0f }
+            new BossConfig { id = NPCID.KingSlime, name = "King Slime", hpMultiplier = 1.5f, strengthMultiplierOverride = 1.2f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.EyeofCthulhu, name = "Eye of Cthulhu", hpMultiplier = 2.0f, strengthMultiplierOverride = 1.5f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.EaterofWorldsHead, name = "Eater of Worlds", hpMultiplier = 1.8f, strengthMultiplierOverride = 1.3f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.BrainofCthulhu, name = "Brain of Cthulhu", hpMultiplier = 1.7f, strengthMultiplierOverride = 1.4f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.QueenBee, name = "Queen Bee", hpMultiplier = 2.0f, strengthMultiplierOverride = 1.6f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.SkeletronHead, name = "Skeletron", hpMultiplier = 2.5f, strengthMultiplierOverride = 2.0f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.WallofFlesh, name = "Wall of Flesh", hpMultiplier = 3.0f, strengthMultiplierOverride = 2.5f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.Retinazer, name = "The Twins (Retinazer)", hpMultiplier = 2.2f, strengthMultiplierOverride = 1.8f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.Spazmatism, name = "The Twins (Spazmatism)", hpMultiplier = 2.2f, strengthMultiplierOverride = 1.8f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.TheDestroyer, name = "The Destroyer", hpMultiplier = 3.0f, strengthMultiplierOverride = 2.0f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.SkeletronPrime, name = "Skeletron Prime", hpMultiplier = 3.5f, strengthMultiplierOverride = 2.5f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.Plantera, name = "Plantera", hpMultiplier = 3.5f, strengthMultiplierOverride = 2.7f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.Golem, name = "Golem", hpMultiplier = 3.0f, strengthMultiplierOverride = 2.2f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.DukeFishron, name = "Duke Fishron", hpMultiplier = 4.0f, strengthMultiplierOverride = 3.0f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.CultistBoss, name = "Lunatic Cultist", hpMultiplier = 4.5f, strengthMultiplierOverride = 3.5f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.MoonLordCore, name = "Moon Lord", hpMultiplier = 5.0f, strengthMultiplierOverride = 4.0f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.QueenSlimeBoss, name = "Queen Slime", hpMultiplier = 3.0f, strengthMultiplierOverride = 2.5f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.HallowBoss, name = "Empress of Light", hpMultiplier = 4.0f, strengthMultiplierOverride = 3.5f sizeScaleOverride = 1.5f },
+            new BossConfig { id = NPCID.Deerclops, name = "Deerclops", hpMultiplier = 2.5f, strengthMultiplierOverride = 2.0f sizeScaleOverride = 1.5f }
         };
 
         string json = JsonConvert.SerializeObject(defaultConfig, Formatting.Indented);
